6676abc3896c3dfbaf9c16129985433183ffa502
Hazem Ahmed <111501299+Hazem-Ahmed1@users.noreply.github.com>
2024-10-03 20:03:58 +0300

    Project-Initialzation

--- portal.sql
+++ portal.sql
@@ -5,7 +5,7 @@ CREATE TABLE Department (
     DepartmentID INT PRIMARY KEY IDENTITY(1,1),
     Name NVARCHAR(50) UNIQUE NOT NULL,
     Code NVARCHAR(10) UNIQUE NOT NULL,
-    HeadID NVARCHAR(20) NULL
+    HeadID char(14) NULL
 );
 
 CREATE TABLE ApplicationUser (
@@ -36,26 +36,26 @@ CREATE TABLE Student (
 CREATE TABLE TeachingAssistant (
     TAID char(14) PRIMARY KEY FOREIGN KEY REFERENCES ApplicationUser(NationalID),
     AcademicDegree NVARCHAR(50) NOT NULL,
-    University NVARCHAR(100) NOT NULL,
-    Faculty NVARCHAR(100) NOT NULL,
+    University NVARCHAR(50) NOT NULL,
+    Faculty NVARCHAR(50) NOT NULL,
     DepartmentID INT FOREIGN KEY REFERENCES Department(DepartmentID),
-    AssistingProfessorID NVARCHAR(20) FOREIGN KEY REFERENCES Person(NationalID)
+    AssistingProfessorID char(14) FOREIGN KEY REFERENCES ApplicationUser(NationalID)
 );
 
 CREATE TABLE Professor (
-    ProfessorID NVARCHAR(20) PRIMARY KEY FOREIGN KEY REFERENCES Person(NationalID),
-    PhDAt NVARCHAR(100) NOT NULL,
+    ProfessorID char(14) PRIMARY KEY FOREIGN KEY REFERENCES ApplicationUser(NationalID),
+    PhDAt NVARCHAR(50) NOT NULL,
     EnterYear INT NOT NULL,
-    OfficeLocation NVARCHAR(100) NOT NULL,
-    DocUni NVARCHAR(100) NOT NULL,
+    OfficeLocation NVARCHAR(50),
+    DocUni NVARCHAR(50) NOT NULL,
     Title NVARCHAR(50) NOT NULL,
     DepartmentID INT FOREIGN KEY REFERENCES Department(DepartmentID)
 );
 
 CREATE TABLE Course (
     CourseID INT PRIMARY KEY IDENTITY(1,1),
-    Name NVARCHAR(100) NOT NULL,
-    Code NVARCHAR(10) UNIQUE NOT NULL,
+    Name NVARCHAR(50) NOT NULL,
+    Code nVARCHAR(10) UNIQUE NOT NULL,
     Hours INT NOT NULL,
     DepartmentID INT FOREIGN KEY REFERENCES Department(DepartmentID)
 );
@@ -65,23 +65,23 @@ CREATE TABLE Task (
     CourseID INT FOREIGN KEY REFERENCES Course(CourseID),
     Grade DECIMAL(4, 2) NOT NULL,
     Deadline DATETIME NOT NULL,
-    Content NVARCHAR(MAX) NOT NULL,
+    Content NVARCHAR(50) NOT NULL,
     Type NVARCHAR(50) CHECK (Type IN ('Project', 'Assignment')) NOT NULL,
-    AssignedByTAID NVARCHAR(20) FOREIGN KEY REFERENCES TeachingAssistant(TAID)
+    AssignedByTAID char(14) FOREIGN KEY REFERENCES TeachingAssistant(TAID)
 );
 
 CREATE TABLE Submission (
     SubmissionID INT PRIMARY KEY IDENTITY(1,1),
     TaskID INT FOREIGN KEY REFERENCES Task(TaskID),
-    StudentID NVARCHAR(20) FOREIGN KEY REFERENCES Student(StudentID),
-    SubmissionLink NVARCHAR(255) NOT NULL,
+    StudentID char(14) FOREIGN KEY REFERENCES Student(NationalID),
+    SubmissionLink NVARCHAR(50) NOT NULL,
     SubmissionDate DATETIME NOT NULL
 );
 
 CREATE TABLE Test (
     TestID INT PRIMARY KEY IDENTITY(1,1),
     TotalGrade DECIMAL(5, 2) NOT NULL,
-    MadeBy NVARCHAR(20) FOREIGN KEY REFERENCES Person(NationalID),
+    MadeBy char(14) FOREIGN KEY REFERENCES ApplicationUser(NationalID),
     CourseID INT FOREIGN KEY REFERENCES Course(CourseID),
     Date DATETIME NOT NULL,
     Time TIME NOT NULL,
@@ -100,50 +100,49 @@ CREATE TABLE Question (
 CREATE TABLE TestSubmission (
     TestSubmissionID INT PRIMARY KEY IDENTITY(1,1),
     TestID INT FOREIGN KEY REFERENCES Test(TestID),
-    StudentID NVARCHAR(20) FOREIGN KEY REFERENCES Student(StudentID),
-    SubmissionLink NVARCHAR(255) NOT NULL,
+    StudentID char(14) FOREIGN KEY REFERENCES Student(NationalID),
     SubmissionDate DATETIME NOT NULL
 );
 
 CREATE TABLE Timetable (
     TimetableID INT PRIMARY KEY IDENTITY(1,1),
     CourseID INT FOREIGN KEY REFERENCES Course(CourseID),
-    ProfessorID NVARCHAR(20) FOREIGN KEY REFERENCES Professor(ProfessorID),
-    Day NVARCHAR(20) NOT NULL,
-    Time TIME NOT NULL,
+    ProfessorID char(14) FOREIGN KEY REFERENCES Professor(ProfessorID),
+    [Day] NVARCHAR(20) NOT NULL,
+    [Time] TIME NOT NULL,
     Duration TIME NOT NULL
 );
 
 CREATE TABLE Notification (
     NotificationID INT PRIMARY KEY IDENTITY(1,1),
-    Message NVARCHAR(MAX) NOT NULL,
-    SentByAdminID NVARCHAR(20) FOREIGN KEY REFERENCES Person(NationalID)
+    [Message] NVARCHAR(50) NOT NULL,
+    SentByAdminID char(14) FOREIGN KEY REFERENCES ApplicationUser(NationalID)
 );
 
 CREATE TABLE Feedback (
     FeedbackID INT PRIMARY KEY IDENTITY(1,1),
-    Question NVARCHAR(MAX) NOT NULL,
-    SubmittedByStudentID NVARCHAR(20) FOREIGN KEY REFERENCES Student(StudentID)
+    Question NVARCHAR(50) NOT NULL,
+    SubmittedByStudentID char(14) FOREIGN KEY REFERENCES Student(NationalID)
 );
 
 CREATE TABLE FeedbackResponse (
     ResponseID INT PRIMARY KEY IDENTITY(1,1),
     FeedbackID INT FOREIGN KEY REFERENCES Feedback(FeedbackID),
-    ResponseText NVARCHAR(MAX) NOT NULL,
-    SubmittedByStudentID NVARCHAR(20) FOREIGN KEY REFERENCES Student(StudentID)
+    ResponseText NVARCHAR(50) NOT NULL,
+    SubmittedByStudentID char(14) FOREIGN KEY REFERENCES Student(NationalID)
 );
 
 CREATE TABLE PasswordResetTicket (
     TicketID INT PRIMARY KEY IDENTITY(1,1),
-    StudentID NVARCHAR(20) FOREIGN KEY REFERENCES Student(StudentID),
+    StudentID char(14) FOREIGN KEY REFERENCES Student(NationalID),
     RequestDate DATETIME NOT NULL,
     ResetDate DATETIME NULL,
-    AdminID NVARCHAR(20) FOREIGN KEY REFERENCES Person(NationalID)
+    AdminID char(14) FOREIGN KEY REFERENCES ApplicationUser(NationalID)
 );
 
 CREATE TABLE CourseEnrollment (
     EnrollmentID INT PRIMARY KEY IDENTITY(1,1),
-    StudentID NVARCHAR(20) FOREIGN KEY REFERENCES Student(StudentID),
+    StudentID char(14) FOREIGN KEY REFERENCES Student(NationalID),
     CourseID INT FOREIGN KEY REFERENCES Course(CourseID),
     EnrollmentDate DATETIME NOT NULL
 );
@@ -151,8 +150,8 @@ CREATE TABLE CourseEnrollment (
 CREATE TABLE CourseTeaching (
     TeachingID INT PRIMARY KEY IDENTITY(1,1),
     CourseID INT FOREIGN KEY REFERENCES Course(CourseID),
-    ProfessorID NVARCHAR(20) NULL FOREIGN KEY REFERENCES Professor(ProfessorID),
-    TAID NVARCHAR(20) NULL FOREIGN KEY REFERENCES TeachingAssistant(TAID)
+    ProfessorID char(14) NULL FOREIGN KEY REFERENCES Professor(ProfessorID),
+    TAID char(14) NULL FOREIGN KEY REFERENCES TeachingAssistant(TAID)
 );
 
 ALTER TABLE Department
