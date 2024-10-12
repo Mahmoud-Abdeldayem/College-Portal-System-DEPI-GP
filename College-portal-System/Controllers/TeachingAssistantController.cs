df621f533d8f4654d406c2b525dffcd99e8f3511
Hazem Ahmed <111501299+Hazem-Ahmed1@users.noreply.github.com>
2024-10-12 16:26:34 +0300

    problem-fixed

--- College-portal-System/Controllers/TeachingAssistantController.cs
+++ College-portal-System/Controllers/TeachingAssistantController.cs
@@ -8,7 +8,7 @@ namespace College_portal_System.Controllers
         // GET: TeachingAssistantController
         public ActionResult Index()
         {
-            return View("Results");
+            return View();
         }
 
         // GET: TeachingAssistantController/Details/5
