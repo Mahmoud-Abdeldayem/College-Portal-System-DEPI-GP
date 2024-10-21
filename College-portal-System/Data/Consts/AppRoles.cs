4ccf859ac13ff80e024494387fe6ccb099aee0b9
Hazem Ahmed <111501299+Hazem-Ahmed1@users.noreply.github.com>
2024-10-21 19:36:11 +0300

    adding Professor View

---
+++ College-portal-System/Data/Consts/AppRoles.cs
@@ -0,0 +1,10 @@
+﻿namespace College_portal_System.Data.Consts
+{
+    public static class AppRoles
+    {
+        public const string Admin = "Admin";
+        public const string TA = "Teaching Assisstant";
+        public const string Student = "Student";
+        public const string Professor = "Professor";
+    }
+}
